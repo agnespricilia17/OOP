4c513475d40a68f4753007bf138b947647645ef8
Agnes Pricilia <agnespricilia17@gmail.com>
2019-05-15 08:20:27 +0700

    base

---
+++ OOP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OOP
+//
+//  Created by agnes pricilia on 15/05/19.
+//  Copyright © 2019 agnes pricilia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
